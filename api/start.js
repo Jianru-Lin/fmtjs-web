43a5811f870f60dca9299372087c171baceb7b8e
Jianru Lin <jianru.lin@gmail.com>
2017-02-09 12:29:21 +0800

    支持后台启动模式

--- api/start.js
+++ api/start.js
@@ -8,8 +8,15 @@ module.exports = function(opt, cb) {
 	var self = this
 
 	opt = opt || {}
-	var host = opt.host || config.host
-	var port = opt.port || config.port
+
+	if (opt.background) {
+		// run in a new process, not this
+		run_in_background(opt, cb)
+		return
+	}
+
+	var host = opt.public ? '0.0.0.0' : config.host
+	var port = config.port
 	dbg('host=' + host + ', port=' + port)
 
 	var onetime_cb = (function() {
@@ -29,3 +36,68 @@ module.exports = function(opt, cb) {
 		onetime_cb(err)
 	})
 }
+
+// # cb(err, status)
+function run_in_background(opt, cb) {
+	// check if it is runing already?
+	is_web_server_started(
+		function yes() {
+			cb(new Error('another instance exists already'))
+		},
+		function no() {
+			opt = JSON.parse(JSON.stringify(opt))
+			delete opt.background
+
+			var path = require('path')
+			var spawn = require('child_process').spawn
+			var child = spawn('node', [__filename, JSON.stringify(opt)], {detached: true})
+			child.stdin.unref()
+			child.stdout.unref()
+			child.stderr.unref()
+			child.unref()
+
+			// wait for a short time
+			// then retrieve status info to check
+			setTimeout(function() {
+				require('../index').status(function(err, status) {
+					assert(!err)
+					cb(undefined, status)
+				})
+			}, 500)
+		}
+	)
+
+	function is_web_server_started(yes_cb, no_cb) {
+		require('../index').status(function(err, status) {
+			if (err) {
+				no_cb()
+			}
+			else {
+				yes_cb()
+			}
+		})
+	}
+}
+
+// trick...
+
+;(function() {
+	if (require.main !== module) return
+
+	if (process.argv.length !== 3) {
+		console.log('Do not invoke this program by hand.')
+		return
+	}
+
+	var opt_str = process.argv[2]
+
+	try {
+		var opt = JSON.parse(opt_str)
+	}
+	catch (err) {
+		console.error('Parsing as JSON failed: ' + opt_str)
+		return
+	}
+
+	require('../index').start(opt)
+})();
