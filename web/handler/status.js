5eadaf03c093676dd6294ace9f29b7462bc6a4f4
Jianru Lin <jianru.lin@gmail.com>
2017-02-09 22:46:44 +0800

    准备实现自动检测 IP

--- web/handler/status.js
+++ web/handler/status.js
@@ -1,11 +1,13 @@
-var config = require('../../config')
+var share = require('../share/')
+var app = require('../index')
+var os = require('os')
 
 module.exports = function(app) {
 	app.get('/status', function(req, res) {
 		res.json({
 			ok: true,
 			version: require('../../package').version,
-			url: config.url(),
+			url: share.url,
 			timestamp: new Date().toISOString()
 		})
 	})
---
+++ web/share/index.js
@@ -0,0 +1,6 @@
+var config = require('../../config')
+
+exports.url = config.url()
+
+// detect public url
+// todo
